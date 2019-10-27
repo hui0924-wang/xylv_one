348fe7b398c6e48e66e726dc865b578e8a18b12e
uksir <yeah126139163@163.com>
2019-10-27 15:17:49 +0800

    注册表单的数据绑定

---
+++ components/user/RegisterForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="register_form">
+    <el-form
+      label-position="top"
+      label-width="80px"
+    >
+      <el-form-item>
+        <el-input  v-model="form.username"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input  v-model="form.nickname" placeholder="请输入内容">
+          <template slot="append">.com</template>
+        </el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input  v-model="form.captcha"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input  v-model="form.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input  v-model="form.password2"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button
+          type="primary"
+          style="width:100%;"
+        >注册</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        username: "1111",
+        nickname: "222",
+        captcha: "333",
+        password: "444",
+        password2: "555"
+      }
+    };
+  }
+};
+</script>
+
+<style>
+.register_form {
+  padding: 20px;
+}
+</style>
--- pages/user/login.vue
+++ pages/user/login.vue
@@ -2,13 +2,19 @@
   <div class="login">
     <div class="login_main">
       <div class="login_main_title">
-        <div class="title_item" :class="currentIndex===index?'active':''"   @click="currentIndex=index" v-for="(item,index) in ['登录','注册']" :key="index"  >{{item}}</div>
+        <div
+          class="title_item"
+          :class="currentIndex===index?'active':''"
+          @click="currentIndex=index"
+          v-for="(item,index) in ['登录','注册']"
+          :key="index"
+        >{{item}}</div>
       </div>
       <div class="login_main_content">
         <!-- 登录组件 -->
-        <div  v-if="currentIndex===0"  >登录</div>
+        <div v-if="currentIndex===0"> <LoginForm/> </div>
         <!-- 注册组件 -->
-        <div v-else>注册</div>
+        <div v-else> <RegisterForm/> </div>
       </div>
 
     </div>
@@ -16,12 +22,19 @@
 </template>
 
 <script>
+import LoginForm from "@/components/user/LoginForm";
+import RegisterForm from "@/components/user/RegisterForm";
+
 export default {
   data() {
     return {
-      currentIndex:0
-    }
+      currentIndex:1
+    };
   },
+  components: {
+    LoginForm,
+    RegisterForm
+  }
 };
 </script>
 
@@ -29,32 +42,32 @@ export default {
 .login {
   height: 700px;
   min-width: 1000px;
-  background: url(http://127.0.1:1337/assets/images/th03.jfif) center center;
+  background: url(http://127.1:1337/assets/images/th03.jfif) center center;
   position: relative;
-  .login_main{
+  .login_main {
     position: absolute;
     background-color: #fff;
     width: 400px;
-    top:50%;
+    top: 50%;
     left: 50%;
-    transform: translate(-50%,-50%);
-    .login_main_title{
+    transform: translate(-50%, -50%);
+    .login_main_title {
       height: 50px;
       color: #666;
       background-color: #eeeeee;
       display: flex;
-      .title_item{
+      .title_item {
         flex: 1;
         display: flex;
         justify-content: center;
         align-items: center;
       }
-      .active{
+      .active {
         color: orange;
         position: relative;
         background-color: #fff;
         font-weight: 600;
-        &::before{
+        &::before {
           content: "";
           position: absolute;
           top: 0;
