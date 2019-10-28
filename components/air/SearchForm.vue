63d163c0b1fb3606c66cd924ded7de5c636a525e
uksir <yeah126139163@163.com>
2019-10-28 11:52:42 +0800

    机票-搜索表单的样式

---
+++ components/air/SearchForm.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="search_form">
+    <div class="search_title">
+      <div
+        class="title_item"
+        v-for="(item,index) in ['单程','往返']"
+        :key="index"
+        :class="currentIndex===index?'active':''"
+      >{{item}}</div>
+    </div>
+    <div class="search_content">
+      <el-form label-width="80px">
+        <el-form-item label="活动名称">
+          <el-autocomplete placeholder="请输入内容"></el-autocomplete>
+        </el-form-item>
+        <!-- 换 -->
+        <div class="city_change">换</div>
+        <!-- 换 -->
+        <el-form-item label="活动名称">
+          <el-autocomplete placeholder="请输入内容"></el-autocomplete>
+        </el-form-item>
+        <el-form-item label="活动时间">
+          <el-date-picker
+            type="date"
+            placeholder="选择日期"
+            style="width: 100%;"
+          ></el-date-picker>
+        </el-form-item>
+        <el-form-item>
+          <el-button
+            style="width: 100%;"
+            type="primary"
+          >立即创建</el-button>
+
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentIndex: 0
+    };
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.search_form {
+}
+
+.search_title {
+  height: 50px;
+  background-color: #fff;
+  display: flex;
+  .title_item {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #eee;
+  }
+  .active {
+    background-color: #fff;
+    position: relative;
+    &::before {
+      content: "";
+      position: absolute;
+      width: 100%;
+      height: 3px;
+      top: 0;
+      left: 0;
+      background-color: orange;
+    }
+  }
+}
+.search_content {
+  padding: 20px 50px 20px 25px;
+  position: relative;
+  .city_change {
+    position: absolute;
+    background-color: #666;
+    width: 22px;
+    height: 22px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    font-size: 13px;
+    right: 11px;
+    top: 59px;
+    &::before {
+      content: "";
+      width: 30px;
+      height: 19px;
+      border: 1px solid #ccc;
+      border-left: none;
+      border-bottom: none;
+      position: absolute;
+      top: -20px;
+      left: -20px;
+    }
+    &::after {
+      content: "";
+      width: 30px;
+      height: 19px;
+      border: 1px solid #ccc;
+      border-left: none;
+      border-top: none;
+      position: absolute;
+      bottom: -20px;
+      left: -20px;
+    }
+  }
+}
+</style>
--- pages/air/index.vue
+++ pages/air/index.vue
@@ -1,13 +1,50 @@
 <template>
-  <div>机票</div>
+  <div class="air_contain">
+    <div class="air_title">
+      <i class="iconfont iconfeiji"></i>
+      国内机票
+    </div>
+    <div class="air_form">
+      <div class="air_search">
+        <SearchForm/>
+      </div>
+      <div class="air_img">
+        <img src="http://157.122.54.189:9093/images/pic_sale.jpeg" alt="">
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import SearchForm from "@/components/air/SearchForm";
 export default {
-
+  components: {
+    SearchForm
+  }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.air_contain{
+  width: 1000px;
+  margin: 0 auto;
+}
+.air_title{
+  padding: 20px 0;
+  color: orange;
+  font-size: 22px;
+  .iconfont{
+    font-size: inherit;
+  }
+}
+.air_form{
+  display: flex;
+  justify-content: space-between;
+  .air_search{
+    width: 360px;
+    border: 1px solid #ccc;
+    height: 350px;
+  }
+}
 
 </style>
