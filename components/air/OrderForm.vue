c6267ab23af4b9f015d30b83b841dc6a06ff0758
uksir <yeah126139163@163.com>
2019-10-31 11:33:30 +0800

    保险数据循环显示

---
+++ components/air/OrderForm.vue
@@ -0,0 +1,115 @@
+<template>
+  <div>
+          <!-- 1 乘机人 -->
+      <div class="fly_people">
+        <div class="fly_people_title">乘机人</div>
+        <div class="fly_people_content">
+          <el-form
+            label-position="top"
+            label-width="80px"
+          >
+            <el-form-item label="乘机人类型">
+              <el-row>
+                <el-col :span="6">
+                  <el-select value="成人" placeholder="">
+                    <el-option
+                      label="成人"
+                      value="成人"
+                    >
+                    </el-option>
+                  </el-select>
+                </el-col>
+                <el-col :span="18">
+                  <el-input></el-input>
+                </el-col>
+              </el-row>
+            </el-form-item>
+            <el-form-item label="证件类型">
+              <el-row>
+                <el-col :span="6">
+                  <el-select value="身份证" placeholder="">
+                    <el-option
+                      label="身份证"
+                      value="身份证"
+                    >
+                    </el-option>
+                  </el-select>
+                </el-col>
+                <el-col :span="18">
+                  <el-input></el-input>
+                </el-col>
+              </el-row>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary">添加乘机人</el-button>
+
+            </el-form-item>
+
+          </el-form>
+        </div>
+      </div>
+      <!-- 1 乘机人 -->
+      <!-- 2 保险 -->
+      <div class="insurance">
+        <div class="insurance_title">保险</div>
+        <div class="insurance_content">
+          <div class="insurance_row" v-for="(item,index) in ticket.insurances" :key="index" >
+            <el-checkbox
+              label="备选项1"
+              border
+            >{{item.type}}：￥{{item.price}}/份×1最高赔付{{item.compensation}}</el-checkbox>
+          </div>
+          
+        </div>
+      </div>
+      <!-- 2 保险 -->
+      <!-- 3 联系人 -->
+      <div class="contact">
+        <div class="contact_title">联系人</div>
+        <div class="contact_content">
+          <el-form label-width="80px">
+            <el-form-item label="姓名">
+              <el-input ></el-input>
+            </el-form-item>
+
+             <el-form-item label="手机">
+              <el-input > <template slot="append">发送验证码</template></el-input>
+            </el-form-item>
+            <el-form-item label="验证码">
+              <el-input ></el-input>
+            </el-form-item>
+           
+            <el-form-item>
+              <el-button
+                type="primary"
+              >立即创建</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
+      </div>
+      <!-- 3 联系人 -->
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    ticket:{
+      type:Object,
+      default:{}
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.fly_people_title,
+.insurance_title,
+.contact_title {
+  padding: 20px 0;
+  font-size: 30px;
+}
+.insurance_row {
+  margin: 10px 0;
+}
+</style>
--- pages/air/Order.vue
+++ pages/air/Order.vue
@@ -1,106 +1,36 @@
 <template>
   <div class="order">
     <div class="order_main">
-      <!-- 1 乘机人 -->
-      <div class="fly_people">
-        <div class="fly_people_title">乘机人</div>
-        <div class="fly_people_content">
-          <el-form
-            label-position="top"
-            label-width="80px"
-          >
-            <el-form-item label="乘机人类型">
-              <el-row>
-                <el-col :span="6">
-                  <el-select placeholder="">
-                    <el-option
-                      label="成人"
-                      value="成人"
-                    >
-                    </el-option>
-                  </el-select>
-                </el-col>
-                <el-col :span="18">
-                  <el-input></el-input>
-                </el-col>
-              </el-row>
-            </el-form-item>
-            <el-form-item label="证件类型">
-              <el-row>
-                <el-col :span="6">
-                  <el-select placeholder="">
-                    <el-option
-                      label="成人"
-                      value="成人"
-                    >
-                    </el-option>
-                  </el-select>
-                </el-col>
-                <el-col :span="18">
-                  <el-input></el-input>
-                </el-col>
-              </el-row>
-            </el-form-item>
-            <el-form-item>
-              <el-button type="primary">立即创建</el-button>
-
-            </el-form-item>
-
-          </el-form>
-        </div>
-      </div>
-      <!-- 1 乘机人 -->
-      <!-- 2 保险 -->
-      <div class="insurance">
-        <div class="insurance_title">保险</div>
-        <div class="insurance_content">
-          <div class="insurance_row">
-            <el-checkbox
-              label="备选项1"
-              border
-            >航空意外险：￥30/份×1最高赔付260万</el-checkbox>
-          </div>
-          <div class="insurance_row">
-            <el-checkbox
-              label="备选项1"
-              border
-            >航空意外险：￥30/份×1最高赔付260万</el-checkbox>
-          </div>
-        </div>
-      </div>
-      <!-- 2 保险 -->
-      <!-- 3 联系人 -->
-      <div class="contact">
-        <div class="insurance_title">联系人</div>
-        <div class="insurance_content">
-          <el-form label-width="80px">
-            <el-form-item label="活动名称">
-              <el-input ></el-input>
-            </el-form-item>
-
-             <el-form-item label="活动名称">
-              <el-input > <template slot="append">.com</template></el-input>
-            </el-form-item>
-            <el-form-item label="活动名称">
-              <el-input ></el-input>
-            </el-form-item>
-           
-            <el-form-item>
-              <el-button
-                type="primary"
-              >立即创建</el-button>
-            </el-form-item>
-          </el-form>
-        </div>
-      </div>
-      <!-- 3 联系人 -->
+      <OrderForm :ticket="ticket" />
     </div>
     <div class="order_aside">2</div>
   </div>
 </template>
 
 <script>
-export default {};
+import OrderForm from "@/components/air/OrderForm";
+export default {
+  data() {
+    return {
+      ticket: {}
+    };
+  },
+  components: {
+    OrderForm
+  },
+  mounted() {
+    // http://127.0.0.1:1337/airs/167?seat_xid=557f1a56db044b4d450ace41fb401d6b
+    this.$axios
+      .get("/airs/" + this.$route.query.id, {
+        params: {
+          seat_xid: this.$route.query.seat_xid
+        }
+      })
+      .then(res => {
+        this.ticket = res.data;
+      });
+  }
+};
 </script>
 
 <style>
@@ -114,14 +44,5 @@ export default {};
 }
 .order_aside {
   flex: 1;
-}
-
-.fly_people_title,
-.insurance_title {
-  padding: 20px 0;
-  font-size: 30px;
-}
-.insurance_row {
-  margin: 10px 0;
 }
 </style>
