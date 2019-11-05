9f58091e9ca7923390db50d8dd5d5c0b685fb724
shubiao5566 <5366814+shubiao5566@user.noreply.gitee.com>
2019-11-05 09:46:52 +0800

    攻略详情内容完成

---
+++ pages/post/detail.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="postdetail">
+    <div class="postdetail_left">
+      <PostdetailLeft />
+    </div>
+    <div class="postdetail_right">
+      2
+    </div>
+  </div>
+</template>
+
+<script>
+import PostdetailLeft from "@/components/air/PostdetailLeft";
+
+export default {
+components: {
+  PostdetailLeft
+}
+}
+</script>
+
+<style lang='less' scoped>
+.postdetail {
+  display: flex;
+  width: 1000px;
+  margin: 0 auto;
+  .postdetail_left {
+    flex: 3;
+  }
+  .postdetail_right {
+    flex: 1;
+  }
+}
+</style>
